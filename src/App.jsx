5a141e96083d893223c38282e60e95574c6f918e
Emrandu1989 <emrandu1989@gmail.com>
2024-03-16 09:47:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+
+function App() {
+  
+
+  return (
+    <>
+      
+       <NavBar />
+     
+    </>
+  )
+}
+
+export default App
